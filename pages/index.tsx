9dafbc658ada7d800c7bf6dabcbb6f0dc14960f3
Joey Bayes <joseph.w.bayes@outlook.com>
2020-11-25 12:41:00 -0500

    Trim down boilerplate styles.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
+import styles from '../styles/pages/Home.module.scss';
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Posts</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
