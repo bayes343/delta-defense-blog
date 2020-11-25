dd39b3f5d56ad9e418de68a613fbbc054f4ea33c
Joey Bayes <joseph.w.bayes@outlook.com>
2020-11-25 12:00:58 -0500

    Setup testing with jest and enzyme.

---
+++ __tests__/pages/index.spec.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import Home from '../../pages/index';
+
+describe('Pages', () => {
+  describe('Index', () => {
+    it('should render without throwing an error', function () {
+      const wrap = mount(<Home />);
+      expect(wrap.find('h1').text()).toBe('Welcome to Next.js!');
+    });
+  });
+});
